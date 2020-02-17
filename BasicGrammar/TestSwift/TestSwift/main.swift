369affe0c3571278c271f2006c042d42dac8d02e
JIANG SHOUDONG <18511632756@163.com>
2020-02-17 14:07:13 +0800

    值类型和引用类型的区别

--- BasicGrammar/TestSwift/TestSwift/main.swift
+++ BasicGrammar/TestSwift/TestSwift/main.swift
@@ -8,20 +8,79 @@
 
 import Foundation
 
-enum Direction {
-    case north, south, east, west
+func testValueType() {
+    
 }
-let d = Direction.north
 
-switch d {
-case .north:
-    print("north")
-case .south:
-    print("south")
-case .east:
-    print("ease")
-case .west:
-    print("west")
+func testClassAndStruct() {
+    class Size {
+        var width = 1
+        var height = 2
+        
+    }
+    struct Point {
+        var x = 3
+        var y = 4
+    }
+    
+    var size = Size()
+    var point = Point()
+    
+}
+
+testClassAndStruct()
+
+func testClass() {
+    class Point {
+        var x = 0
+        var y = 0
+        
+    }
+    
+    let _ = Point()
+    
+}
+func testStruct() {
+    struct Point {
+        var x = 10
+        var y = 20
+        var b = true
+    }
+    let _ = Point()
+    print(MemoryLayout<Point>.size)
+    print(MemoryLayout<Point>.stride)
+    print(MemoryLayout<Point>.alignment)
+}
+
+
+/// 测试枚举
+func testEnum2() {
+    enum TestEnum {
+        case test1(Int, Int, Int)
+        case test2(Int, Int)
+        case test3(Int)
+        case test4(Bool)
+        case test5
+    }
+    
+    var e = TestEnum.test1(10, 20, 30)
+//    print(Mems.ptr(ofVal: &e))
+    switch e {
+    case let .test1(v1, v2, v3):
+        print("test1", v1, v2, v3)
+        
+    case let .test2(v1, v2):
+        print("test2", v1, v2)
+        
+    case let .test3(v1):
+        print("test3", v1)
+        
+    case let .test4(v1):
+        print("test4", v1)
+        
+    case .test5:
+        print("test5")
+    }
 }
 
 
